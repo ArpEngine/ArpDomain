1929719a3eb98b15e00555461500cf0bc6b98429
kaikoga <info@mail.kaikoga.net>
2019-09-16 08:59:46 +0900

    do not fallback to default factory

--- src/arp/domain/factory/ArpObjectFactory.hx
+++ src/arp/domain/factory/ArpObjectFactory.hx
@@ -24,7 +24,7 @@ class ArpObjectFactory<T:IArpObject> {
 	public function matchSeed(seed:ArpSeed, type:ArpType, className:String):Float {
 		if (type != this.arpType) return -1;
 		if (className == this.className) return 100;
-		if (isDefault) return 1;
+		if (className == null && isDefault) return 1;
 		return -1;
 	}
 
