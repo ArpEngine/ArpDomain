0fa5dff18de88b05c12e713ebd5728d12c0eafe7
kaikoga <info@mail.kaikoga.net>
2020-06-30 01:28:18 +0900

    cosme

--- src/arp/errors/loadErrors/ArpVoidFactoryError.hx
+++ src/arp/errors/loadErrors/ArpVoidFactoryError.hx
@@ -2,7 +2,7 @@ package arp.errors.loadErrors;
 
 class ArpVoidFactoryError extends ArpLoadError {
 	override private function get_recoverInfo():String {
-		return "ArpVoidReferenceError: Please check the structure of your seed files.";
+		return "ArpVoidFactoryError: Please check the structure of your seed files.";
 	}
 
 	public function new(message:String, cause:ArpError = null) {
